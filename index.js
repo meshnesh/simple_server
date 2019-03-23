f7c4da9df40d1a97785fb837b65608d7c58a0233
meshnesh <tonnie.nesh@gmail.com>
2019-03-23 20:42:53 +0300

    change message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 // A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Eric'))
 
 // Start the Express server
 app.listen(3000, () => console.log('Server running on port 3000!')) 
